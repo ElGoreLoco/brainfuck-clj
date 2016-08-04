a61d7564ff6b7109c63ca5e8d900607c7f0766d9
ElGoreLoco <esegoreloco@gmail.com>
2016-08-04 16:45:12 +0200

    Move button actions to separate functions.

--- src/brainfuck_clj/ui.clj
+++ src/brainfuck_clj/ui.clj
@@ -4,6 +4,20 @@
 
 (defn make-ui
   [program-data program-paused]
+
+  (defn on-run [_]
+    (a/set-agent program-paused false))
+
+  (defn on-stop [_]
+    (a/set-agent program-paused true))
+
+  (defn on-reload [_]
+    (if @program-paused
+      (do
+        (println) ; FIXME: this will be replaced with cleaning the gui buffer.
+        (a/setup-program-data program-data (apply str (:instructions @program-data))))
+      (alert "The interpreter must be paused to be able to reload the program.")))
+
   (native!)
   (def f
     (frame
@@ -28,18 +42,11 @@
                                      :align :left)
                                    (flow-panel
                                      :items [(button :text "Run"
-                                                     :listen [:action (fn [_] (a/set-agent program-paused false))])
+                                                     :listen [:action on-run])
                                              (button :text "Stop"
-                                                     :listen [:action (fn [_] (a/set-agent program-paused true))])
+                                                     :listen [:action on-stop])
                                              (button :text "Reload"
-                                                     :listen [:action (fn [_]
-                                                                        (if @program-paused
-                                                                          (do
-                                                                            (println)
-                                                                            (a/setup-program-data
-                                                                              program-data
-                                                                              (apply str (:instructions @program-data))))
-                                                                          (alert "The interpreter must be paused to be able to reload the program.")))])]
+                                                     :listen [:action on-reload])]
                                      :align :right)])
                          (scrollable
                            (label :text "" :id :cells)
